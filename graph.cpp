ec31d68ae3d34af0dc26cdbe6543a4f9352af7ca
Habis <habis-code@proton.me>
2026-03-18 17:16:29 +0100

    repairing

--- graph.cpp
+++ graph.cpp
@@ -33,40 +33,47 @@ struct GraphInfo{
 struct Graph{
     vector<GraphInfo> info; 
     vector<vector<pair<int, int>>> pair_graph; 
-    bool direct; 
 };
 
 namespace JsonFill {
-    
-    const validatorMap validators = {
+
+    const validatorMap VALIDATORS = {
         {"ArticleName", [](const json& j){ return j.is_string(); }},
-        {"Number",      [](const json& j){ return j.is_number_integer(); }},
-        {"Neighbours",  [](const json& j){ 
-            if (!j.is_array()) return false;
-            for (const auto& el : j)
-                if (!el.is_number_integer()) return false;
-            return true;
-        }},
         {"Tags",        [](const json& j){ 
             if (!j.is_array()) return false;
             for (const auto& el : j) 
                 if (!el.is_string()) return false;
             return true;
-            }},
-        {"Direct",      [](const json& j){ return j.is_boolean(); }}
+            }}
+    };
+
+    const validatorMap CRYTICAL_VALIDATORS = {
+        {"Number",      [](const json& j){ return j.is_number_integer(); }}
     };
 
     void typesValidation(const json& article, Valid& results, size_t i){
-        for (const auto& [key, value] : validators) {
+        for (const auto& [key, value] : VALIDATORS) {
             if (!article.contains(key)) {
                 results.crytical_errors.push_back(
-                    "Brakuje pola: " + key + "w obiekcie nr." + to_string(i) + 
-                    "\nNie można zbudować grafu");
+                    "\tBrakuje pola: '" + key + "' w obiekcie nr." + to_string(i) + 
+                    "\n");
                 continue;
             }
             if (!value(article.at(key))) {
-                results.errors.push_back("Niewłaściwy typ pola: " + key + "w obiekcie nr. " + to_string(i) +
-                " mogą wystąpić błędy!\n Sprzwdź dokumentacje o tworzeniu pliku json do budowania grafów");
+                results.errors.push_back("\tNiewłaściwa wartość pola: '" + key + "' w obiekcie nr. " + to_string(i) +
+                " mogą wystąpić błędy!\n\tSprzwdź dokumentacje o tworzeniu pliku json do budowania grafów");
+            }
+        }
+        for (const auto& [key, value] : CRYTICAL_VALIDATORS) {
+            if (!article.contains(key)) {
+                results.crytical_errors.push_back(
+                    "\tBrakuje pola: '" + key + "' w obiekcie nr." + to_string(i) + 
+                    "\n");
+                continue;
+            }
+            if (!value(article.at(key))) {
+                results.crytical_errors.push_back("\tNiewłaściwa wartość pola: '" + key + "' w obiekcie nr. " + to_string(i) +
+                " błąd krytyczny!\n");
             }
         }
     }
@@ -98,15 +105,17 @@ namespace JsonFill {
             typesValidation(article, results, i);
         }
 
-        unordered_set<int> numbers;
-        for (const auto& element : data) 
-            numbers.insert(element["Number"].get<int>());
-
-        for (size_t i = 0; i < data.size(); i++) {
-            if (!numbers.count(i)) {
-                results.crytical_errors.push_back(
-                    "Numery wierzchołków muszą być kolejne od 0! Brakuje wierzchołka nr. " + to_string(i)
-                );
+        if (results.build()) {
+            unordered_set<int> numbers;
+            for (const auto& element : data) 
+                numbers.insert(element["Number"].get<int>());
+            
+            for (size_t i = 0; i < data.size(); i++) {
+                if (!numbers.count(i)) {
+                    results.crytical_errors.push_back(
+                        "Numery wierzchołków muszą być kolejne od 0! Brakuje wierzchołka nr. " + to_string(i)
+                    );
+                }
             }
         }
 
@@ -114,13 +123,23 @@ namespace JsonFill {
     } 
 
     bool checkErrors(const Valid& results){
+        if (results.build()) {
+            cout << "Crytical names - ok\n" << endl;
+        } else {
+            cout << "Found some crytical errors!\n\tBecause of this graph can't be build.\n\nCrytical Errors:\n";
+            for (const auto& e : results.crytical_errors) {
+                cout << e ;
+            }
+            cout << "\n";
+        }
         if (results.ok()) {
             cout << "Configuration's values - ok\n" << endl;
         } else {
-            cout << "Found some errors!\n Because of this graph may doesn't work.\n Errors:\n";
-            for (const auto e : results.errors) {
+            cout << "Found some errors!\n\tBecause of this graph may doesn't work.\n\nErrors:\n";
+            for (const auto& e : results.errors) {
                 cout << e ;
             }
+            cout << "\n";
         }
         if (results.build()) return false;
         return true; 
@@ -128,148 +147,93 @@ namespace JsonFill {
 
     Graph buildGraph(const string& json_file){
         Valid results = validation(json_file);
+        // validation of json file
         if (checkErrors(results)) throw runtime_error("Json File is corrupted!");
+        // printing errors if there is any
 
-        Graph graph;
+        Graph article_graph;
+        // variable to store information about graph
         ifstream file(json_file);
+        // reading file
         json data;
+        // creating container for our json file to parse
         file >> data;
+        // automated parsing
 
-        graph.pair_graph.resize(data.size());
-        graph.info.resize(data.size());
-        graph.direct = data[0]["Direct"];
+        article_graph.pair_graph.resize(data.size());
+        article_graph.info.resize(data.size());
         
         for (const auto& element : data) {
             int idx = element["Number"];
-            graph.info[idx].article_name = element["ArticleName"];
+            article_graph.info[idx].article_name = element["ArticleName"];
 
-            for (const auto& tag : element["Tags"]) {
-                graph.info[idx].tags.push_back(tag);
-            }
+            for (const auto& tag : element["Tags"])
+                article_graph.info[idx].tags.push_back(tag);
+        }
 
-            for (const auto& num : element["Neighbours"]) {
-                graph.pair_graph.at(idx).push_back({num, 0});
+        for (size_t i = 0; i < article_graph.info.size(); i++) {
+            // firt loop to check with other object for commmon tags
+
+            unordered_set<string> tags_A(article_graph.info[i].tags.begin(), article_graph.info[i].tags.end());
+            // fast comparison of tags with indexes made from tags
+
+            for (size_t j = i + 1; j < article_graph.info.size(); j++){
+                //second loop to compare tagc from first loop object
+                int common = 0;
+                // common is containing points form common tags and indicate if edge is created
+
+                for (const auto& tag_B : article_graph.info[j].tags) {
+                    // looping tags from second object
+                    if (tags_A.count(tag_B)) common++;
+                    // checking tags from 2 obcjets
+                }
+
+                    if (common > 0) {
+                        // if there is any common tags we're creating two edges for each one
+                        article_graph.pair_graph[i].push_back({j,common});
+                        article_graph.pair_graph[j].push_back({i,common});
+                    }
             }
         }
-        return graph;
+
+        return article_graph;
     }
 }
 
 namespace GraphTools {
-
-    void dfs(int start, Graph& g, vector<int>& visited){
-        visited[start] = 1;
-
-        for (auto& pair_neighbor : g.pair_graph[start]) {
-            if (visited[pair_neighbor.first] == -1) dfs(pair_neighbor.first, g, visited);
-        }
-    } 
-    
-    void bfs(int start, Graph& g, vector<int>& distances, queue<int>& graph_queue){ 
-        graph_queue.push(start);
-        distances[start] = 0;
-
-        while (!graph_queue.empty()) {
-            int v = graph_queue.front(); 
-            graph_queue.pop();
-
-            for (auto& vertex : g.pair_graph[v]) {
-                
-                if (distances[vertex.first] == -1) {
-                    graph_queue.push(vertex.first);
-                    distances[vertex.first] = distances[v] + 1;
-                }
-            }
-        }
-    }
-
-    void dijkstra(){
-        throw runtime_error("not implemented");
-    }
-    
-    void weight(int first, int second, Graph& g, bool direct){
-
-        // Algorithm assigning weight based on tags related to articles
-
-        unordered_set<string> set_a(g.info[first].tags.begin(), g.info[first].tags.end());
-        // Using 'set_a' to compare tags using mash instead of array (fastter)
-        // I make set_a importing information from 'g.info[vertex_of_the_graph'
-        // that instead of regular indexes uses mash which converts string 
-        // to sha number used in index
-
-        int common = 0, only_b = 0;
-
-        for (auto& b : g.info[second].tags) {
-            if (set_a.count(b)) {
-                common++;
-            } else {
-                only_b++;
-            }
-        }
-        // Determines if the tag is common or not and stores it in variables.
-
-        int only_a = g.info[first].tags.size() - common;
-        // Value of the uncommon tags from article A
-
-        for (auto& edge : g.pair_graph[first]) {
-            if (edge.first == second) {
-                edge.second = common - only_a - only_b;
-                break;
-            }
-        }
-        
-        if (!g.direct) {
-            for (auto& edge : g.pair_graph[second]) {
-                if (edge.first == first) {
-                    edge.second = common - only_a - only_b;
-                    break;
-                }
-            }
-        }
-    }
-
-    void fillWeight(Graph& g){
-        for (size_t x = 0; x < g.pair_graph.size(); x++) {
-            // For each vertex of graph is doing 'for loop'
-            for (auto& pair : g.pair_graph[x]) {
-                // It gets the pair container to pair variable
-
-                int y = pair.first;
-                // Variable 'y' takes the neighbour number
-
-                if (!g.direct && x > y) continue;   
-                // pomiń duplikat
-
-                weight(x, y, g, g.direct);
-                // Execution of 'weight()' function to fill up the weight
-            }
-        }
+    vector<pair<int,int>> recommend(int article, Graph& article_graph) {
+        vector<pair<int,int>> neighbours = article_graph.pair_graph[article];
+        sort(neighbours.begin(), neighbours.end(), [](const pair<int,int>& a, const pair<int,int>& b){
+            return a.second > b.second; // malejąco po wadze
+        });
+        return neighbours;
     }
 }
 
-int main(){
-    // Graph graph;
-    // vector<int> visited(graph.pair_graph.size(), -1);
-    // vector<int> distans(graph.pair_graph.size(), -1);
-    // queue<int> graph_queue;
-    // how to implement the graph
+int main(int argc, char *argv[]){
+    // command line arguments
+    string file = argc > 1 ? argv[1] : "graph.json";
+    // check if file path is given or not 
+    int article_num = argc > 2 ? stoi(argv[2]) : 0;
 
     try {
-        Graph graph = JsonFill::buildGraph("graph_disconnected.json");
-        GraphTools::fillWeight(graph);
+        Graph graph = JsonFill::buildGraph(file);
+        // building the graph 
+        auto recommendations = GraphTools::recommend(article_num, graph);
+        cout << "Recommendations for: " << graph.info[0].article_name << endl;
+        for (auto& rec : recommendations)
+            cout << graph.info[rec.first].article_name << " (similarity: " << rec.second << ")" << endl;
 
-        vector<int> visited(graph.pair_graph.size(), -1);
-        GraphTools::dfs(0, graph, visited);
-
-        vector<int> distances(graph.pair_graph.size(), -1);
-        queue<int> graph_queue;
-        GraphTools::bfs(0, graph, distances, graph_queue);
+        for (size_t i = 0; i < graph.pair_graph.size(); i++) {
+            cout << graph.info[i].article_name << " -> ";
+            for (auto& edge : graph.pair_graph[i])
+                cout << graph.info[edge.first].article_name << " (w:" << edge.second << ") ";
+            cout << endl;
+        }
 
     } catch (const runtime_error& e) {
         cout << e.what() << endl;
     }
-
-    
     
     return 0;
 }
